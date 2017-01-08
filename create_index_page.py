65f3cbeeb7b0d29e4dd6e3f7148d37add379868a
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2017-01-08 23:27:08 +0400

    Сохранение индексного файла в папку html

--- create_index_page.py
+++ create_index_page.py
@@ -1,5 +1,14 @@
 import jinja2
-from convert import form_path_to_save_html, get_config, save_html_file
+import os
+from convert import get_config, save_html_file
+
+
+def form_article_path(markdown_path):
+    path_to_save = os.path.dirname(markdown_path)
+    extension = '.html'
+    new_filename = os.path.splitext(os.path.basename(markdown_path))[0] + extension
+    final_path = os.path.join(path_to_save, new_filename)
+    return final_path
 
 
 if __name__ == '__main__':
@@ -8,7 +17,8 @@ if __name__ == '__main__':
         template = template_file.read()
 
     html_jinja_template = jinja2.Template(template)
-    html_jinja_template.globals['form_path_to_save_html'] = form_path_to_save_html
-    result_html = html_jinja_template.render(topics=config['topics'],
+    html_jinja_template.globals['form_article_path'] = form_article_path
+    result_html = html_jinja_template.render(text="Взлом с помощью </body>",
+                                             topics=config['topics'],
                                              articles=config['articles'])
-    save_html_file(result_html, 'index.html')
+    save_html_file(result_html, './html/index.html')
